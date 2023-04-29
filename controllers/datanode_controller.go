d4eace590788587cb1d13a49ca65f217bc4227e3
Amir Allahveran <amir79allahvern@gmail.com>
2023-04-29 10:32:34 +0330

    Add datanode controller

---
+++ controllers/datanode_controller.go
@@ -0,0 +1,296 @@
+package controllers
+
+import (
+	"context"
+	"github.com/AmirAllahveran/HDFS-operator/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// DesiredDataNodeConfigMap
+// The jmx?qry=Hadoop:service=DataNode,name=DataNodeInfo path is a query string used to access the JMX
+// (Java Management Extensions) interface of the DataNode in an HDFS cluster.
+// JMX is a Java technology that provides a standard way to manage and monitor Java applications.
+// Hadoop exposes several metrics and management operations through JMX. The JMX interface is exposed through an
+// HTTP endpoint on each Hadoop component, including DataNodes.
+// In this specific query:
+// service=DataNode specifies that you want to access the JMX information for the DataNode service.
+// name=DataNodeInfo specifies that you want to access the DataNodeInfo MBean (Management Bean),
+// which provides information about the DataNode.
+// When you send an HTTP request with this query string to the DataNode's JMX HTTP endpoint
+// (typically on port 50075 or 1006), it will return a JSON object containing the DataNodeInfo MBean's attributes,
+// such as the cluster ID, volume failures, and other details about the DataNode's status.
+func (r *HDFSClusterReconciler) desiredDataNodeConfigMap(hdfsCluster *v1alpha1.HDFSCluster) (*corev1.ConfigMap, error) {
+	dataNodeConfigMapTemplate := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      hdfsCluster.Name + "-datanode-script",
+			Namespace: hdfsCluster.Namespace,
+			Labels: map[string]string{
+				"cluster":   hdfsCluster.Name,
+				"app":       "hdfsCluster",
+				"component": "datanode",
+			},
+		},
+		Data: map[string]string{
+			"check-status.sh": `#!/usr/bin/env bash
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+set -o xtrace
+_PORTS="50075 1006"
+_URL_PATH="jmx?qry=Hadoop:service=DataNode,name=DataNodeInfo"
+_CLUSTER_ID=""
+for _PORT in $_PORTS; do
+  _CLUSTER_ID+=$(curl -s http://localhost:${_PORT}/$_URL_PATH |  \
+      grep ClusterId) || true
+done
+echo $_CLUSTER_ID | grep -q -v null`,
+		},
+	}
+
+	if err := ctrl.SetControllerReference(hdfsCluster, dataNodeConfigMapTemplate, r.Scheme); err != nil {
+		return dataNodeConfigMapTemplate, err
+	}
+
+	return dataNodeConfigMapTemplate, nil
+}
+
+func (r *HDFSClusterReconciler) desiredDataNodeStatefulSet(hdfsCluster *v1alpha1.HDFSCluster) (*appsv1.StatefulSet, error) {
+	stsTemplate := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      hdfsCluster.Name + "-datanode",
+			Namespace: hdfsCluster.Namespace,
+			Labels: map[string]string{
+				"cluster":   hdfsCluster.Name,
+				"app":       "hdfsCluster",
+				"component": "datanode",
+			},
+		},
+		Spec: appsv1.StatefulSetSpec{
+			ServiceName: hdfsCluster.Name + "-datanode",
+			Replicas:    stringToInt32(hdfsCluster.Spec.DataNode.Replicas),
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"cluster":   hdfsCluster.Name,
+						"app":       "hdfsCluster",
+						"component": "datanode",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "hdfs-datanode",
+							Image: "uhopper/hadoop-datanode:2.7.2",
+							LivenessProbe: &corev1.Probe{
+								ProbeHandler: corev1.ProbeHandler{
+									Exec: &corev1.ExecAction{
+										Command: []string{
+											"/scripts/check-status.sh",
+										},
+									},
+								},
+								InitialDelaySeconds: 60,
+								PeriodSeconds:       30,
+							},
+							ReadinessProbe: &corev1.Probe{
+								ProbeHandler: corev1.ProbeHandler{
+									Exec: &corev1.ExecAction{
+										Command: []string{
+											"/scripts/check-status.sh",
+										},
+									},
+								},
+								InitialDelaySeconds: 60,
+								PeriodSeconds:       30,
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "hdfs-site",
+									MountPath: "/opt/hadoop/etc/hadoop/hdfs-site.xml",
+									SubPath:   "hdfs-site.xml",
+								},
+								{
+									Name:      "core-site",
+									MountPath: "/opt/hadoop/etc/hadoop/core-site.xml",
+									SubPath:   "core-site.xml",
+								},
+								{
+									Name:      "datanode-script",
+									MountPath: "/scripts/check-status.sh",
+									SubPath:   "check-status.sh",
+								},
+								{
+									Name:      hdfsCluster.Name,
+									MountPath: "/data/hadoop",
+								},
+							},
+						},
+					},
+					RestartPolicy: corev1.RestartPolicyAlways,
+					Volumes: []corev1.Volume{
+						{
+							Name: "hdfs-site",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: hdfsCluster.Name + "-config",
+									},
+									Items: []corev1.KeyToPath{
+										{
+											Key:  "hdfs-site.xml",
+											Path: "hdfs-site.xml",
+										},
+									},
+								},
+							},
+						},
+						{
+							Name: "core-site",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: hdfsCluster.Name + "-config",
+									},
+									Items: []corev1.KeyToPath{
+										{
+											Key:  "core-site.xml",
+											Path: "core-site.xml",
+										},
+									},
+								},
+							},
+						},
+						{
+							Name: "datanode-script",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: hdfsCluster.Name + "-datanode-script",
+									},
+									Items: []corev1.KeyToPath{
+										{
+											Key:  "check-status.sh",
+											Path: "check-status.sh",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: hdfsCluster.Name,
+					},
+					Spec: corev1.PersistentVolumeClaimSpec{
+						AccessModes: []corev1.PersistentVolumeAccessMode{
+							corev1.ReadWriteOnce,
+						},
+						Resources: corev1.ResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceStorage: resource.MustParse(hdfsCluster.Spec.DataNode.Resources.Storage),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := ctrl.SetControllerReference(hdfsCluster, stsTemplate, r.Scheme); err != nil {
+		return stsTemplate, err
+	}
+
+	return stsTemplate, nil
+}
+
+//func (r *HDFSClusterReconciler) dataNodesExist(ctx context.Context, hdfsCluster *v1alpha1.HDFSCluster) (bool, error) {
+//	// Define the desired DataNode StatefulSet object
+//	desiredStatefulSet, _ := r.desiredDataNodeStatefulSet(hdfsCluster)
+//
+//	// Check if the StatefulSet already exists
+//	existingStatefulSet := &appsv1.StatefulSet{}
+//	err := r.Get(ctx, client.ObjectKeyFromObject(desiredStatefulSet), existingStatefulSet)
+//
+//	if err != nil {
+//		if errors.IsNotFound(err) {
+//			return false, nil
+//		}
+//		return false, err
+//	}
+//
+//	// Check if all DataNode pods are in Running state
+//	replicas := desiredStatefulSet.Status.Replicas
+//	for i := int32(0); i < replicas; i++ {
+//		dataNodePod := &corev1.Pod{}
+//		dataNodePodName := types.NamespacedName{
+//			Namespace: hdfsCluster.Namespace,
+//			Name:      fmt.Sprintf("%s-%d", desiredStatefulSet.Name, i),
+//		}
+//		err = r.Get(ctx, dataNodePodName, dataNodePod)
+//
+//		if err != nil {
+//			if errors.IsNotFound(err) {
+//				return false, nil
+//			}
+//			return false, err
+//		}
+//
+//		if dataNodePod.Status.Phase != corev1.PodRunning {
+//			return false, nil
+//		}
+//	}
+//
+//	return true, nil
+//}
+
+func (r *HDFSClusterReconciler) createOrUpdateDataNode(ctx context.Context, hdfs *v1alpha1.HDFSCluster) error {
+	// Define the desired NameNode Service object
+	desiredDAtaNodeConfigMap, _ := r.desiredDataNodeConfigMap(hdfs)
+	desiredDataNodeStatefulSet, _ := r.desiredDataNodeStatefulSet(hdfs)
+
+	// Check if the datanode already exists
+
+	// Check if the Service already exists
+	existingConfigMap := &corev1.ConfigMap{}
+	err := r.Get(ctx, client.ObjectKeyFromObject(desiredDAtaNodeConfigMap), existingConfigMap)
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	// Create or update the Service
+	if errors.IsNotFound(err) {
+		if err := r.Create(ctx, desiredDAtaNodeConfigMap); err != nil {
+			return err
+		}
+	}
+
+	existingStatefulSet := &appsv1.StatefulSet{}
+	err = r.Get(ctx, client.ObjectKeyFromObject(desiredDataNodeStatefulSet), existingStatefulSet)
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	// Create or update the Service
+	if errors.IsNotFound(err) {
+		if err := r.Create(ctx, desiredDataNodeStatefulSet); err != nil {
+			return err
+		}
+	} else {
+		existingStatefulSet.Spec = desiredDataNodeStatefulSet.Spec
+		if err := r.Update(ctx, existingStatefulSet); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ controllers/datanode_controller_test.go
@@ -0,0 +1,214 @@
+package controllers
+
+import (
+	"context"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"reflect"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"testing"
+
+	"github.com/AmirAllahveran/HDFS-operator/api/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestDesiredDataNodeConfigMap(t *testing.T) {
+
+	// Set up a fake client to mock API calls
+	s := runtime.NewScheme()
+	_ = corev1.AddToScheme(s)
+	_ = v1alpha1.AddToScheme(s) // Add your custom resource to the scheme
+
+	// Create a HDFSCluster object
+	hdfsCluster := &v1alpha1.HDFSCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "hdfs-cluster",
+			Namespace: "default",
+		},
+	}
+
+	// Create a Reconciler instance with the fake client
+	r := &HDFSClusterReconciler{
+		Client: fake.NewClientBuilder().WithScheme(s).WithObjects(hdfsCluster).Build(),
+		Scheme: s,
+	}
+
+	configMap, _ := r.desiredDataNodeConfigMap(hdfsCluster)
+
+	expectedName := hdfsCluster.Name
+	expectedNamespace := hdfsCluster.Namespace
+	expectedLabels := map[string]string{
+		"app": hdfsCluster.Name,
+	}
+
+	if configMap.Name != expectedName {
+		t.Errorf("Expected name to be %s, but got %s", expectedName, configMap.Name)
+	}
+
+	if configMap.Namespace != expectedNamespace {
+		t.Errorf("Expected namespace to be %s, but got %s", expectedNamespace, configMap.Namespace)
+	}
+
+	for key, value := range expectedLabels {
+		if configMap.Labels[key] != value {
+			t.Errorf("Expected label %s to be %s, but got %s", key, value, configMap.Labels[key])
+		}
+	}
+
+	expectedData := map[string]string{
+		"check-status.sh": `#!/usr/bin/env bash
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+set -o xtrace
+_PORTS="50075 1006"
+_URL_PATH="jmx?qry=Hadoop:service=DataNode,name=DataNodeInfo"
+_CLUSTER_ID=""
+for _PORT in $_PORTS; do
+  _CLUSTER_ID+=$(curl -s http://localhost:${_PORT}/$_URL_PATH |  \
+      grep ClusterId) || true
+done
+echo $_CLUSTER_ID | grep -q -v null`,
+	}
+
+	for key, value := range expectedData {
+		if configMap.Data[key] != value {
+			t.Errorf("Expected data for key %s to be %s, but got %s", key, value, configMap.Data[key])
+		}
+	}
+
+	// Check that the ConfigMap owner reference has been set correctly
+	if len(configMap.OwnerReferences) != 1 || configMap.OwnerReferences[0].UID != hdfsCluster.UID {
+		t.Fatal("unexpected owner reference in ConfigMap")
+	}
+}
+
+func TestHDFSClusterReconciler_createOrUpdateDataNode(t *testing.T) {
+	// Set up a fake client to mock API calls
+	s := runtime.NewScheme()
+	_ = corev1.AddToScheme(s)
+	_ = appsv1.AddToScheme(s)
+	_ = v1alpha1.AddToScheme(s) // Add your custom resource to the scheme
+
+	// Create a HDFSCluster object
+	hdfsCluster := &v1alpha1.HDFSCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "hdfs-cluster",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.HDFSClusterSpec{
+			DataNode: v1alpha1.DataNode{
+				Resources: v1alpha1.Resources{
+					Storage: "10Gi",
+				},
+			},
+		},
+	}
+
+	// Create a Reconciler instance with the fake client
+	r := &HDFSClusterReconciler{
+		Client: fake.NewClientBuilder().WithScheme(s).WithObjects(hdfsCluster).Build(),
+		Scheme: s,
+	}
+
+	// Call the method under test
+	err := r.createOrUpdateDataNode(context.Background(), hdfsCluster)
+	if err != nil {
+		t.Fatalf("Failed to create or update DataNode: %v", err)
+	}
+
+	// Check that the ConfigMap and StatefulSet were created or updated
+	configMap := &corev1.ConfigMap{}
+	err = r.Get(context.Background(), client.ObjectKey{
+		Namespace: hdfsCluster.Namespace,
+		Name:      hdfsCluster.Name + "-datanode-script",
+	}, configMap)
+	if err != nil {
+		t.Errorf("Failed to get ConfigMap: %v", err)
+	}
+
+	statefulSet := &appsv1.StatefulSet{}
+	err = r.Get(context.Background(), client.ObjectKey{
+		Namespace: hdfsCluster.Namespace,
+		Name:      hdfsCluster.Name + "-datanode",
+	}, statefulSet)
+	if err != nil {
+		t.Errorf("Failed to get StatefulSet: %v", err)
+	}
+}
+
+func TestHDFSClusterReconciler_desiredDataNodeStatefulSet(t *testing.T) {
+
+	// Set up a fake client to mock API calls
+	s := runtime.NewScheme()
+	_ = corev1.AddToScheme(s)
+	_ = appsv1.AddToScheme(s)
+	_ = v1alpha1.AddToScheme(s) // Add your custom resource to the scheme
+
+	// Initialize a mock HDFSCluster
+	hdfs := &v1alpha1.HDFSCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-hdfs",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.HDFSClusterSpec{
+			DataNode: v1alpha1.DataNode{
+				Replicas: "3",
+				Resources: v1alpha1.Resources{
+					Storage: "1Gi",
+				},
+			},
+		},
+	}
+
+	// Create a Reconciler instance with the fake client
+	r := &HDFSClusterReconciler{
+		Client: fake.NewClientBuilder().WithScheme(s).WithObjects(hdfs).Build(),
+		Scheme: s,
+	}
+
+	sts, err := r.desiredDataNodeStatefulSet(hdfs)
+	if err != nil {
+		t.Fatalf("expected no error, got: %v", err)
+	}
+
+	// Check the StatefulSet name
+	if sts.Name != "test-hdfs-datanode" {
+		t.Errorf("expected name to be 'test-hdfs-datanode', got: %s", sts.Name)
+	}
+
+	// Check the StatefulSet namespace
+	if sts.Namespace != "default" {
+		t.Errorf("expected namespace to be 'default', got: %s", sts.Namespace)
+	}
+
+	// Check the number of replicas
+	if *sts.Spec.Replicas != 3 {
+		t.Errorf("expected 3 replicas, got: %d", *sts.Spec.Replicas)
+	}
+
+	// Check the StatefulSet labels
+	expectedLabels := map[string]string{
+		"cluster":   "test-hdfs",
+		"app":       "hdfsCluster",
+		"component": "datanode",
+	}
+	if !reflect.DeepEqual(sts.Labels, expectedLabels) {
+		t.Errorf("expected labels to be '%v', got: %v", expectedLabels, sts.Labels)
+	}
+
+	// Check the container image
+	if sts.Spec.Template.Spec.Containers[0].Image != "uhopper/hadoop-datanode:2.7.2" {
+		t.Errorf("expected image to be 'uhopper/hadoop-datanode:2.7.2', got: %s", sts.Spec.Template.Spec.Containers[0].Image)
+	}
+
+	//// Check the volume claim templates
+	//expectedStorage := resource.MustParse(hdfs.Spec.DataNode.Resources.Storage)
+	//if sts.Spec.VolumeClaimTemplates[0].Spec.Resources.Requests[corev1.ResourceStorage].Equal(expectedStorage) {
+	//	t.Errorf("expected storage to be '%s', got: %s", expectedStorage.String(),
+	//		sts.Spec.VolumeClaimTemplates[0].Spec.Resources.Requests[corev1.ResourceStorage])
+	//}
+}
