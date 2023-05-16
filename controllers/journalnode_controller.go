ce4686e49a85bdc8ac1d0cc1423e22356f1dcc3e
Amir Allahveran <amir79allahvern@gmail.com>
2023-05-16 17:11:44 +0330

    Add Journalnode controller

---
+++ controllers/journalnode_controller.go
@@ -0,0 +1,275 @@
+package controllers
+
+import (
+	"context"
+	"github.com/AmirAllahveran/HDFS-operator/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/api/policy/v1beta1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"strconv"
+)
+
+func (r *HDFSClusterReconciler) desiredJournalNodePodDisruptionBudget(hdfs *v1alpha1.HDFSCluster) (*v1beta1.PodDisruptionBudget, error) {
+	replicas, _ := strconv.ParseInt(hdfs.Spec.JournalNode.Replicas, 10, 32)
+	minAvailable := replicas/2 + 1
+	pdbTemplate := &v1beta1.PodDisruptionBudget{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      hdfs.Name + "-journalnode",
+			Namespace: hdfs.Namespace,
+		},
+		Spec: v1beta1.PodDisruptionBudgetSpec{
+			MinAvailable: &intstr.IntOrString{
+				IntVal: int32(minAvailable),
+			},
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"cluster":   hdfs.Name,
+					"app":       "hdfsCluster",
+					"component": "journalnode",
+				},
+			},
+		},
+	}
+
+	if err := ctrl.SetControllerReference(hdfs, pdbTemplate, r.Scheme); err != nil {
+		return pdbTemplate, err
+	}
+
+	return pdbTemplate, nil
+}
+
+func (r *HDFSClusterReconciler) desiredJournalNodeService(hdfsCluster *v1alpha1.HDFSCluster) (*corev1.Service, error) {
+	svcTemplate := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: hdfsCluster.Namespace,
+			Name:      hdfsCluster.Name + "-journalnode",
+			Labels: map[string]string{
+				"cluster":   hdfsCluster.Name,
+				"app":       "hdfsCluster",
+				"component": "journalnode",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "http",
+					Port:       8480,
+					TargetPort: intstr.FromString("http"),
+				},
+				{
+					Name:       "ipc",
+					Port:       8485,
+					TargetPort: intstr.FromString("ipc"),
+				},
+			},
+			ClusterIP: corev1.ClusterIPNone,
+			Selector: map[string]string{
+				"cluster":   hdfsCluster.Name,
+				"app":       "hdfsCluster",
+				"component": "journalnode",
+			},
+		},
+	}
+
+	if err := ctrl.SetControllerReference(hdfsCluster, svcTemplate, r.Scheme); err != nil {
+		return svcTemplate, err
+	}
+
+	return svcTemplate, nil
+}
+
+func (r *HDFSClusterReconciler) createOrUpdateJournalNode(ctx context.Context, hdfs *v1alpha1.HDFSCluster) error {
+
+	desiredJournalNodeService, _ := r.desiredJournalNodeService(hdfs)
+	desiredJournalNodePodDisruptionBudget, _ := r.desiredJournalNodePodDisruptionBudget(hdfs)
+	desiredJournalNodeStatefulSet, _ := r.desiredJournalNodeStatefulSet(hdfs)
+
+	// Check if the journal already exists
+
+	// Check if the Service already exists
+	existingService := &corev1.Service{}
+	err := r.Get(ctx, client.ObjectKeyFromObject(desiredJournalNodeService), existingService)
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	// Create or update the Service
+	if errors.IsNotFound(err) {
+		if err := r.Create(ctx, desiredJournalNodeService); err != nil {
+			return err
+		}
+	}
+
+	// Check if the PodDisruptionBudget already exists
+	existingPodDisruptionBudget := &v1beta1.PodDisruptionBudget{}
+	err = r.Get(ctx, client.ObjectKeyFromObject(desiredJournalNodePodDisruptionBudget), existingPodDisruptionBudget)
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	// Create or update the PodDisruptionBudget
+	if errors.IsNotFound(err) {
+		if err := r.Create(ctx, desiredJournalNodePodDisruptionBudget); err != nil {
+			return err
+		}
+	}
+
+	existingStatefulSet := &appsv1.StatefulSet{}
+	err = r.Get(ctx, client.ObjectKeyFromObject(desiredJournalNodeStatefulSet), existingStatefulSet)
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	// Create or update the sts
+	if errors.IsNotFound(err) {
+		if err := r.Create(ctx, desiredJournalNodeStatefulSet); err != nil {
+			return err
+		}
+	} else {
+		existingStatefulSet.Spec = desiredJournalNodeStatefulSet.Spec
+		if err := r.Update(ctx, existingStatefulSet); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *HDFSClusterReconciler) desiredJournalNodeStatefulSet(hdfsCluster *v1alpha1.HDFSCluster) (*appsv1.StatefulSet, error) {
+	stsTemplate := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      hdfsCluster.Name + "-journalnode",
+			Namespace: hdfsCluster.Namespace,
+			Labels: map[string]string{
+				"cluster":   hdfsCluster.Name,
+				"app":       "hdfsCluster",
+				"component": "journalnode",
+			},
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"cluster":   hdfsCluster.Name,
+					"app":       "hdfsCluster",
+					"component": "journalnode",
+				},
+			},
+			ServiceName: hdfsCluster.Name + "-journalnode",
+			Replicas:    stringToInt32(hdfsCluster.Spec.JournalNode.Replicas),
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"cluster":   hdfsCluster.Name,
+						"app":       "hdfsCluster",
+						"component": "journalnode",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "hdfs-journalnode",
+							Image: "amiralh4/journalnode:3.3.1",
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http",
+									ContainerPort: 8480,
+								},
+								{
+									Name:          "ipc",
+									ContainerPort: 8485,
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "hdfs-site",
+									MountPath: "/opt/hadoop/etc/hadoop/hdfs-site.xml",
+									SubPath:   "hdfs-site.xml",
+								},
+								{
+									Name:      "core-site",
+									MountPath: "/opt/hadoop/etc/hadoop/core-site.xml",
+									SubPath:   "core-site.xml",
+								},
+								{
+									Name:      hdfsCluster.Name + "-journalnode",
+									MountPath: "/data/hadoop/journalnode",
+								},
+							},
+						},
+					},
+					RestartPolicy: corev1.RestartPolicyAlways,
+					Volumes: []corev1.Volume{
+						{
+							Name: "hdfs-site",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: hdfsCluster.Name + "-cluster-config",
+									},
+									Items: []corev1.KeyToPath{
+										{
+											Key:  "hdfs-site.xml",
+											Path: "hdfs-site.xml",
+										},
+									},
+								},
+							},
+						},
+						{
+							Name: "core-site",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: hdfsCluster.Name + "-cluster-config",
+									},
+									Items: []corev1.KeyToPath{
+										{
+											Key:  "core-site.xml",
+											Path: "core-site.xml",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: hdfsCluster.Name + "-journalnode",
+					},
+					Spec: corev1.PersistentVolumeClaimSpec{
+						AccessModes: []corev1.PersistentVolumeAccessMode{
+							corev1.ReadWriteOnce,
+						},
+						Resources: corev1.ResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceStorage: resource.MustParse(hdfsCluster.Spec.JournalNode.Resources.Storage),
+							},
+						},
+						//Selector: &metav1.LabelSelector{
+						//	MatchLabels: map[string]string{
+						//		"cluster":   hdfsCluster.Name,
+						//		"app":       "hdfsCluster",
+						//		"component": "journalnode",
+						//	},
+						//},
+					},
+				},
+			},
+		},
+	}
+
+	if err := ctrl.SetControllerReference(hdfsCluster, stsTemplate, r.Scheme); err != nil {
+		return stsTemplate, err
+	}
+
+	return stsTemplate, nil
+}
---
+++ controllers/journalnode_controller_test.go
@@ -0,0 +1,243 @@
+package controllers
+
+import (
+	"context"
+	"github.com/AmirAllahveran/HDFS-operator/api/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/api/policy/v1beta1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"strconv"
+	"testing"
+)
+
+func TestDesiredJournalNodePodDisruptionBudget(t *testing.T) {
+	// Set up a fake client to mock API calls
+	s := runtime.NewScheme()
+	_ = corev1.AddToScheme(s)
+	_ = appsv1.AddToScheme(s)
+	_ = v1alpha1.AddToScheme(s) // Add your custom resource to the scheme
+
+	hdfs := &v1alpha1.HDFSCluster{
+		Spec: v1alpha1.HDFSClusterSpec{
+			NameNode: v1alpha1.NameNode{
+				Replicas: "2",
+			},
+			JournalNode: v1alpha1.JournalNode{
+				Replicas: "3",
+			},
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "testCluster",
+			Namespace: "testNamespace",
+		},
+	}
+
+	// Create a Reconciler instance with the fake client
+	r := &HDFSClusterReconciler{
+		Client: fake.NewClientBuilder().WithScheme(s).WithObjects(hdfs).Build(),
+		Scheme: s,
+	}
+
+	// Act
+	pdb, err := r.desiredJournalNodePodDisruptionBudget(hdfs)
+
+	// Assert
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	replicas, _ := strconv.ParseInt(hdfs.Spec.JournalNode.Replicas, 10, 32)
+	expectedMinAvailable := replicas/2 + 1
+	if pdb.Spec.MinAvailable.IntVal != int32(expectedMinAvailable) {
+		t.Errorf("Expected MinAvailable to be %d but got %d", expectedMinAvailable, pdb.Spec.MinAvailable.IntVal)
+	}
+
+	expectedName := hdfs.Name + "-journalnode"
+	if pdb.ObjectMeta.Name != expectedName {
+		t.Errorf("Expected Name to be %s but got %s", expectedName, pdb.ObjectMeta.Name)
+	}
+
+	expectedNamespace := hdfs.Namespace
+	if pdb.ObjectMeta.Namespace != expectedNamespace {
+		t.Errorf("Expected Namespace to be %s but got %s", expectedNamespace, pdb.ObjectMeta.Namespace)
+	}
+
+	if pdb.Spec.Selector.MatchLabels["cluster"] != hdfs.Name || pdb.Spec.Selector.MatchLabels["app"] != "hdfsCluster" || pdb.Spec.Selector.MatchLabels["component"] != "journalnode" {
+		t.Errorf("Unexpected labels in PodDisruptionBudget Selector")
+	}
+}
+
+func TestHDFSClusterReconciler_desiredJournalNodeService(t *testing.T) {
+	// Set up a fake client to mock API calls
+	s := runtime.NewScheme()
+	_ = corev1.AddToScheme(s)
+	//_ = appsv1.AddToScheme(s)
+	_ = v1alpha1.AddToScheme(s) // Add your custom resource to the scheme
+
+	// Initialize a mock HDFSCluster
+	hdfsCluster := &v1alpha1.HDFSCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-hdfs",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.HDFSClusterSpec{
+			NameNode: v1alpha1.NameNode{
+				Replicas: "2",
+				Resources: v1alpha1.Resources{
+					Storage: "1Gi",
+				},
+			},
+			JournalNode: v1alpha1.JournalNode{
+				Replicas: "1",
+				Resources: v1alpha1.Resources{
+					Storage: "1Gi",
+				},
+			},
+		},
+	}
+
+	// Create a Reconciler instance with the fake client
+	r := &HDFSClusterReconciler{
+		Client: fake.NewClientBuilder().WithScheme(s).WithObjects(hdfsCluster).Build(),
+		Scheme: s,
+	}
+
+	svc, err := r.desiredJournalNodeService(hdfsCluster)
+	assert.Nil(t, err)
+
+	expectedName := hdfsCluster.Name + "-journalnode"
+	assert.Equal(t, expectedName, svc.Name)
+
+	expectedNamespace := hdfsCluster.Namespace
+	assert.Equal(t, expectedNamespace, svc.Namespace)
+
+	expectedLabels := map[string]string{
+		"cluster":   hdfsCluster.Name,
+		"app":       "hdfsCluster",
+		"component": "journalnode",
+	}
+	assert.Equal(t, expectedLabels, svc.Labels)
+
+	expectedPorts := []corev1.ServicePort{
+		{
+			Name:       "http",
+			Port:       8480,
+			TargetPort: intstr.FromString("http"),
+		},
+		{
+			Name:       "ipc",
+			Port:       8485,
+			TargetPort: intstr.FromString("ipc"),
+		},
+	}
+	assert.Equal(t, expectedPorts, svc.Spec.Ports)
+}
+
+func TestHDFSClusterReconciler_createOrUpdateJournalNode(t *testing.T) {
+	// Set up a fake client to mock API calls
+	s := runtime.NewScheme()
+	_ = corev1.AddToScheme(s)
+	_ = appsv1.AddToScheme(s)
+	_ = v1beta1.AddToScheme(s)
+	_ = v1alpha1.AddToScheme(s) // Add your custom resource to the scheme
+
+	// Create a HDFSCluster object
+	hdfs := &v1alpha1.HDFSCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "hdfs-cluster",
+			Namespace: "default",
+		},
+		Spec: v1alpha1.HDFSClusterSpec{
+			NameNode: v1alpha1.NameNode{
+				Replicas: "2",
+				Resources: v1alpha1.Resources{
+					Storage: "10Gi",
+				},
+			},
+			JournalNode: v1alpha1.JournalNode{
+				Replicas: "3",
+				Resources: v1alpha1.Resources{
+					Storage: "3G",
+				},
+			},
+		},
+	}
+
+	// Create a Reconciler instance with the fake client
+	r := &HDFSClusterReconciler{
+		Client: fake.NewClientBuilder().WithScheme(s).WithObjects(hdfs).Build(),
+		Scheme: s,
+	}
+
+	ctx := context.Background()
+	// Act
+	err := r.createOrUpdateJournalNode(ctx, hdfs)
+
+	// Assert
+	assert.NoError(t, err)
+
+	// Check if the Service, PodDisruptionBudget, and StatefulSet were created
+	service := &corev1.Service{}
+	err = r.Get(ctx, types.NamespacedName{Name: hdfs.Name + "-journalnode", Namespace: hdfs.Namespace}, service)
+	assert.NoError(t, err)
+
+	pdb := &v1beta1.PodDisruptionBudget{}
+	err = r.Get(ctx, types.NamespacedName{Name: hdfs.Name + "-journalnode", Namespace: hdfs.Namespace}, pdb)
+	assert.NoError(t, err)
+
+	statefulSet := &appsv1.StatefulSet{}
+	err = r.Get(ctx, types.NamespacedName{Name: hdfs.Name + "-journalnode", Namespace: hdfs.Namespace}, statefulSet)
+	assert.NoError(t, err)
+}
+
+func TestHDFSClusterReconciler_desiredJournalNodeStatefulSet(t *testing.T) {
+
+	// Set up a fake client to mock API calls
+	s := runtime.NewScheme()
+	_ = corev1.AddToScheme(s)
+	_ = appsv1.AddToScheme(s)
+	_ = v1alpha1.AddToScheme(s) // Add your custom resource to the scheme
+
+	hdfs := &v1alpha1.HDFSCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "testCluster",
+			Namespace: "testNamespace",
+		},
+		Spec: v1alpha1.HDFSClusterSpec{
+			JournalNode: v1alpha1.JournalNode{
+				Replicas: "3",
+				Resources: v1alpha1.Resources{
+					Storage: "10Gi",
+				},
+			},
+		},
+	}
+	// Create a Reconciler instance with the fake client
+	r := &HDFSClusterReconciler{
+		Client: fake.NewClientBuilder().WithScheme(s).WithObjects(hdfs).Build(),
+		Scheme: s,
+	}
+
+	// Act
+	sts, err := r.desiredJournalNodeStatefulSet(hdfs)
+
+	// Assert
+	assert.NoError(t, err)
+	assert.NotNil(t, sts)
+	assert.Equal(t, hdfs.Name+"-journalnode", sts.Name)
+	assert.Equal(t, hdfs.Namespace, sts.Namespace)
+
+	// Check the volume claim templates
+	expectedStorage := resource.MustParse(hdfs.Spec.JournalNode.Resources.Storage)
+	if sts.Spec.VolumeClaimTemplates[0].Spec.Resources.Requests.Storage().String() != expectedStorage.String() {
+		t.Errorf("expected storage to be '%s', got: '%s'", hdfs.Spec.DataNode.Resources.Storage,
+			sts.Spec.VolumeClaimTemplates[0].Spec.Resources.Requests.Storage().String())
+	}
+}
