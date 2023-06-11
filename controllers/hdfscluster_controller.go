5f702b3b2478266617dd29a6577d989bb9955b3a
Amir Allahveran <amir79allahvern@gmail.com>
2023-06-12 00:10:06 +0330

    Add v1.PodDisruptionBudget access

--- controllers/hdfscluster_controller.go
+++ controllers/hdfscluster_controller.go
@@ -41,6 +41,7 @@ type HDFSClusterReconciler struct {
 //+kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="policy",resources=poddisruptionbudgets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
